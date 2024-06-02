73025cc7410a9bffea49332ad6445a625716b473
KIGAMEKUN <reksa.prayoga1012@gmail.com>
2024-06-02 20:42:36 +0700

    feat(add dashboard)

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\{Zakat, Infaq, Blog, Activity};
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class DashboardController extends Controller
 {
     public function dashboard(Request $request)
