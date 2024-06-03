bbd786584f1f9ffbe484990e5b25dfb0e2965c31
KIGAMEKUN <reksa.prayoga1012@gmail.com>
2024-06-03 17:29:04 +0700

    add zakat filter

--- app/Http/Controllers/PrayerTimeController.php
+++ app/Http/Controllers/PrayerTimeController.php
@@ -10,7 +10,7 @@ class PrayerTimeController extends Controller
 
     public function index()
     {
-        $url = "http://api.aladhan.com/v1/timingsByCity?city=Jakarta&country=Indonesia&method=8";
+        $url = "http://api.aladhan.com/v1/timingsByCity?city=".$_GET['city']."&country=Indonesia&method=8";
         $data = file_get_contents($url);
         $prayerTime = json_decode($data, true);
         return response()->json($prayerTime);
